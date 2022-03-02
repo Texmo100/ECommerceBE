6fc5d0251ab282982b76d814fb1340b450fbd54d
Marco Aldana <marco.aldana@3pillarglobal.com>
2022-03-02 11:24:38 -0600

    Add Fields and validations to Users, Add migration to database

---
+++ ECommerceBE/Data/Migrations/20220302171735_AddingUserFields.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ECommerceBE.Migrations
+{
+    public partial class AddingUserFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Suppliers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Enabled",
+                table: "Suppliers",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEmailConfirmed",
+                table: "Suppliers",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Customers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Enabled",
+                table: "Customers",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEmailConfirmed",
+                table: "Customers",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Admins",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Enabled",
+                table: "Admins",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEmailConfirmed",
+                table: "Admins",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "Enabled",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "IsEmailConfirmed",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Enabled",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "IsEmailConfirmed",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Admins");
+
+            migrationBuilder.DropColumn(
+                name: "Enabled",
+                table: "Admins");
+
+            migrationBuilder.DropColumn(
+                name: "IsEmailConfirmed",
+                table: "Admins");
+        }
+    }
+}

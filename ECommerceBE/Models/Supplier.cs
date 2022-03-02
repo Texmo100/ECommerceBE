f1baa72d351fefc591db6bc5fbf297b806bc24ce
Marco Aldana <marco.aldana@3pillarglobal.com>
2022-03-02 11:15:28 -0600

    .

--- ECommerceBE/Models/Supplier.cs
+++ ECommerceBE/Models/Supplier.cs
@@ -17,7 +17,7 @@ namespace ECommerceBE.Models
         public string Address { get; set; }
         [Phone]
         public string PhoneNumber { get; set; }
-        public bool IsEmailConfirmed { get; set; } = false;
+        public bool IsEmailConfirmed { get; set; } = true;//TODO change this field when email validation is available
         public bool Enabled { get; set; } = true;
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }

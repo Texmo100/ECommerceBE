8432c7e3368643f8095a455025e6261f5f54a530
Marco Aldana <marco.aldana@3pillarglobal.com>
2022-03-02 11:01:15 -0600

    Added Email and phone validation to IUser

--- ECommerceBE/Interfaces/IUser.cs
+++ ECommerceBE/Interfaces/IUser.cs
@@ -11,8 +11,10 @@ namespace ECommerceBE.Interfaces
         public string Name { get; set; }
         [Required]
         public string Password { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
         public string Address { get; set; }
+        [Phone]
         public string PhoneNumber { get; set; }
     }
 }
